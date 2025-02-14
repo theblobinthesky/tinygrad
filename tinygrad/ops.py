c44c53a00899fc00ec20de3197508d7b08332d2f
Erik Stern <erik.stern@outlook.de>
2025-02-14 16:08:31 +0100

    Removed empty line.

--- tinygrad/ops.py
+++ tinygrad/ops.py
@@ -688,7 +688,6 @@ python_alu: dict[Ops, Callable]  = {
 def exec_alu(op:Ops, dtype:DType, operands, truncate_output=True):
   if dtype.count > 1:
     return tuple([exec_alu(op, dtype.scalar(), [x[i] if isinstance(x, tuple) else x for x in operands]) for i in range(dtype.count)])
-
   alu = python_alu[op](*operands)
   return truncate.get(dtype, lambda x: x)(alu) if truncate_output else alu
 
